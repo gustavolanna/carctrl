6fe1e5f984ad0b349fef9c472b570f7d5de37c6a
yc26gl1 <gustavo.lanna@macys.com>
2018-01-15 18:18:25 -0200

    CC-6 Removendo lib de log

--- server/config/express.js
+++ server/config/express.js
@@ -6,7 +6,6 @@
 
 var express = require('express');
 var favicon = require('serve-favicon');
-var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -38,7 +37,6 @@ module.exports = function(app) {
         app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
         app.use(express.static(path.join(config.root, 'public')));
         app.set('appPath', config.root + '/public');
-        app.use(morgan('dev'));
     }
 
     if ('development' === env || 'test' === env) {
@@ -46,7 +44,6 @@ module.exports = function(app) {
         app.use(express.static(path.join(config.root, '.tmp')));
         app.use(express.static(path.join(config.root, 'client')));
         app.set('appPath', 'client');
-        app.use(morgan('dev'));
         app.use(errorHandler()); // Error handler - has to be last
     }
 };
