1f1f5f08b3885ad9d0f16e5777de8e0b36382e14
Gustavo Lanna <gu.lanna@gmail.com>
2019-03-24 16:10:38 -0700

    CAR-12 - removing setEncoding from express middleware

--- server/config/express.js
+++ server/config/express.js
@@ -23,17 +23,16 @@ module.exports = function(app) {
     app.use(compression());
     app.use(function(req, res, next) {
       req.rawBody = '';
-      req.setEncoding('utf8');
-    
+ 
       req.on('data', function(chunk) { 
         req.rawBody += chunk;
       });
     
       req.on('end', function() {
-        console.log(req.rawBody);
         next();
       });
     });
+
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(methodOverride());
