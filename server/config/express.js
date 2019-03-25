4865725a1a9203b6dd2b734e9a2f98ec807561e5
Gustavo Lanna <gu.lanna@gmail.com>
2019-03-24 18:27:17 -0700

    CAR-12 Disable bodyparser interceptor

--- server/config/express.js
+++ server/config/express.js
@@ -21,18 +21,6 @@ module.exports = function(app) {
     app.engine('html', require('ejs').renderFile);
     app.set('view engine', 'html');
     app.use(compression());
-    // app.use(function(req, res, next) {
-    //   req.rawBody = '';
- 
-    //   req.on('data', function(chunk) { 
-    //     req.rawBody += chunk;
-    //   });
-    
-    //   req.on('end', function() {
-    //     next();
-    //   });
-    // });
-
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(methodOverride());
